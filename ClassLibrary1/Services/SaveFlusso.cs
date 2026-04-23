b52509981c08edd09c9761e49c1a6ac9329fa0f9
Ebrahim1125 <ebrahim.attia.111@gmail.com>
2026-04-23 14:47:05 +0200

    Refactor SaveFlusso2DB error handling and bulk insert

--- ClassLibrary1/Services/SaveFlusso.cs
+++ ClassLibrary1/Services/SaveFlusso.cs
@@ -327,26 +327,26 @@ namespace Vendita.HubMisureEE.Services
                 {
                     HubLog.SaveLog2DB("Error", "SaveFlusso2DB", $"Errore durante la creazione della DataRow quartini per il POD {pod.Pod}: {ex.Message}", connessione);
                 }
-
-                try
-                {
-                    DataRow drFile = FileXml.NewRow();
-                    drFile["DataIns"] = DateTime.Now;
-                    drFile["NomeFile"] = fileName;
-                    drFile["FileXml"] = "";
-                    drFile["Lavorato"] = true;
-                    FileXml.Rows.Add(drFile);
-                }
-                catch (Exception ex)
-                {
-                    HubLog.SaveLog2DB("Error", "SaveFlusso2DB", $"Errore durante la creazione della DataRow FileXml per il file {fileName}: {ex.Message}", connessione);
-                }
-                //Scrittura col bulk
-                Bulk2DB(QE, "Curve", connessione);
-                Bulk2DB(dtLetture, "Letture", connessione);
-                Bulk2DB(FileXml, "FileXml", connessione);
-                FileLavorato(FolderLavoro, fileName, connessione);
             }
+
+            try
+            {
+                DataRow drFile = FileXml.NewRow();
+                drFile["DataIns"] = DateTime.Now;
+                drFile["NomeFile"] = fileName;
+                drFile["FileXml"] = "";
+                drFile["Lavorato"] = true;
+                FileXml.Rows.Add(drFile);
+            }
+            catch (Exception ex)
+            {
+                HubLog.SaveLog2DB("Error", "SaveFlusso2DB", $"Errore durante la creazione della DataRow FileXml per il file {fileName}: {ex.Message}", connessione);
+            }
+            //Scrittura col bulk
+            Bulk2DB(QE, "Curve", connessione);
+            Bulk2DB(dtLetture, "Letture", connessione);
+            Bulk2DB(FileXml, "FileXml", connessione);
+            FileLavorato(FolderLavoro, fileName, connessione);
         }
 
         // Lavorazione del flusso Rettifico e salvataggio su DB
@@ -547,7 +547,7 @@ namespace Vendita.HubMisureEE.Services
                 dr["Pod"] = pod.Pod;
                 nPod = pod.Pod;
                 dr["MeseAnno"] = ParseMonthYearOrDbNull(pod.MeseAnno);
-                
+
                 dr["DataMisura"] = ParseDateOrDbNull(pod?.DataMisura);
 
                 DataMisure = dr["DataMisura"];
